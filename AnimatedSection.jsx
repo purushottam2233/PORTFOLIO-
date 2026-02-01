e1114a69ff02edaba93b7b8fc745bba3a83f90ce
Purushottam Sharma <purushottamsharma83066@gmail.com>
2026-02-01 17:33:28 +0530

    Added index.html

---
+++ AnimatedSection.jsx
@@ -0,0 +1,15 @@
+// function AnimatedSection({ children }) {
+// return (
+// <motion.section
+// variants={fadeUp}
+// initial="hidden"
+// whileInView="visible"
+// viewport={{ once: true }}
+// transition={{ duration: 0.5 }}
+// className="snap-section hover-lift mb-5"
+// >
+// {children}
+// </motion.section>
+// );
+// }
+// export default AnimatedSection;
